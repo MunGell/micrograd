67b4ff321f28a3ff9b6be194acf5cf6c1a916e49
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2026-07-13 10:18:48 +0000

    Let Neurons take full parameters

--- v1/neuron.ts
+++ v1/neuron.ts
@@ -1,21 +1,23 @@
 import Operand from './operand';
 
 class Neuron {
+    parameters: Operand[] = [];
     weigths: Operand[] = [];
     bias: Operand = 0;
     x: Operand[] = [];
     activation: Operand;
 
-    constructor(weights: Operand[], bias: Operand) {
-        this.weights = weights;
-        this.bias = bias;
+    constructor(parameters: Operand[]) {
+        this.parameters = parameters;
+        this.weights = parameters.slice(0, -1);
+        this.bias = parameters.at(-1);
     }
 
     activate(x: Operand[]) {
         if (x.length !== this.weights.length) {
             throw new Error('weights and input lengths mismatch');
         }
-        this.activation = this.weight
+        this.activation = this.weights
             .map((w, i) => w.multiplyBy(x[i])) // map multiplies weight with input
             .reduce((a, v) => a.add(v),0)      // then reduce sums it all up
             .add(this.bias);                   // then we add bias
