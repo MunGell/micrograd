6a767c53000b8effd02246c0b796db3d371d2ffc
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2026-07-13 21:03:52 +0000

    Update neuron.ts and add layer.ts

---
+++ v1/layer.ts
@@ -0,0 +1,21 @@
+import Operand from './operand.ts';
+import Neuron from './neuron.ts';
+
+class Layer {
+    parameters: Operand[][] = [[]];
+    neurons: Neuron[] = [];
+    activations: Operand[] = [];
+
+    constructor(parameters: Operand[][]) {
+        this.parameters = parameters;
+        this.neurons = parameters.map(p => new Neuron(p));
+    }
+
+    activate(input: Operand[]) {
+        this.activations = this.neurons.map((n, i) => n.activate(input));
+
+        return this.activations;
+    }
+}
+
+export default Layer;
--- v1/neuron.ts
+++ v1/neuron.ts
@@ -1,31 +1,28 @@
-import Operand from './operand';
+import Operand from './operand.ts';
 
 class Neuron {
-    parameters: Operand[] = [];
-    weigths: Operand[] = [];
-    bias: Operand = 0;
-    x: Operand[] = [];
-    activation: Operand;
+    parameters: Operand[];
+    weights: Operand[];
+    bias: Operand;
+    x?: Operand[];
+    activation?: Operand;
 
     constructor(parameters: Operand[]) {
         this.parameters = parameters;
         this.weights = parameters.slice(0, -1);
-        this.bias = parameters.at(-1);
+        this.bias = parameters.at(-1)!;
     }
 
-    activate(x: Operand[]) {
-        if (x.length !== this.weights.length) {
+    activate(input: Operand[]) {
+        if (input.length !== this.weights.length) {
             throw new Error('weights and input lengths mismatch');
         }
         this.activation = this.weights
-            .map((w, i) => w.multiplyBy(x[i])) // map multiplies weight with input
-            .reduce((a, v) => a.add(v),0)      // then reduce sums it all up
-            .add(this.bias);                   // then we add bias
-        return this;
-    }
+            .map((w, i) => w.multiplyBy(input[i]))       // multiplies weights with inputs
+            .reduce((a, v) => a.add(v), new Operand(0))  // then sum it all up
+            .add(this.bias);                             // then add bias
 
-    relu() {
-        return this.activation.relu();
+        return this.activation;
     }
 }
 
